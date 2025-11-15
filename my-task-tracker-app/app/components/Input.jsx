08f0eac26fd3dd32d310d53cef97877f6b5e61c3
Nosa <egharevbanosa58@gmail.com>
2025-11-15 08:56:07 -0900

    overall updates

--- my-task-tracker-app/app/components/Input.jsx
+++ my-task-tracker-app/app/components/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { useEffect } from 'react';
 // import Buttons from './Buttons';
 
 
@@ -7,6 +8,22 @@ const Input = () => {
     const [task, setTask] = useState("");
     const [tasks, setTasks] = useState([]);
     const [filter, setFilter] = useState("all");
+
+
+    // Loading tasks on first render
+    useEffect(()=> {
+        const saved = localStorage.getItem("tasks");
+        if (saved) setTasks(JSON.parse(saved));
+    }, []);
+
+
+    //Saving tasks anytime they change
+    useEffect(()=> {
+        if (tasks.length > 0 ){
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+    }, [tasks]);
+
 
 
     //The function to add a task
@@ -56,7 +73,7 @@ const Input = () => {
                     {filteredTasks.map(t => (<li key={t.id} className={`task-container ${t.completed ? "completed" : ""}`}>
                         <input className="tick" type="checkbox" checked={t.completed} onChange={() => check(t.id)} />
 
-                        <span className={`list-div {t.completed ? "completed" : ""}`}>{t.text}</span>
+                        <span className={`list-div ${t.completed ? "completed" : ""}`}>{t.text}</span>
 
                         <button onClick={() => deleteTask(t.id)} className="trash">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash2 h-4 w-4"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
